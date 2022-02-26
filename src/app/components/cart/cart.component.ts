416d8c98b9c0a82aa96ce15087754fb1ab70d354
ethan <ethannguyen.uts@gmail.com>
2022-02-27 00:09:26 +1100

    Added reponsive design

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -10,20 +10,11 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  user: User = new User();
   listProduct: Product[] = [];
   total: number = 0;
   constructor(private cartService: CartService, private router: Router) {}
 
   ngOnInit(): void {
-    this.cartService.addToCart(5, {
-      quantity: 1,
-      name: 'Book',
-      price: 9.99,
-      url: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-      description: 'You can read it!',
-    });
-
     this.updateCart();
   }
 
@@ -43,9 +34,7 @@ export class CartComponent implements OnInit {
     this.listProduct = this.cartService.refreshCart();
     this.updateCart();
   };
-  onSubmit = () => {
-    this.cartService.setUser(this.user);
-    this.cartService.setStatus(CART_STATUS.complete);
-    this.router.navigateByUrl('/confirmation');
+  onCheckout = () => {
+    this.router.navigateByUrl('/checkout');
   };
 }
